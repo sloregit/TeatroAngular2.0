728fd4cc56b0c904e9f2ad0ac8f3abb5a451f095
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-21 22:43:00 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { AppDBService } from './app.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor(private Appservice: AppDBService) {}
+  mostraPrenotazioni() {
+    this.Appservice.getPrenotazioni$().subscribe({
+      next: (res: string) => {
+        console.log(res);
+      },
+      error: (err) =>
+        console.error('Observer got an error: ' + JSON.stringify(err)),
+    });
+  }
 }
