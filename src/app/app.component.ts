cdc96b74a0852ce42ff9bd7c3519aaf7f90c18f5
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-22 12:23:31 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,60 @@
 import { Component, VERSION } from '@angular/core';
 import { AppDataService } from './appdata.service';
 
+export interface datiIn {
+  prenotazioni: string;
+  teatro: Teatro;
+}
+export interface prenotazioni {
+  platea: Array<string>;
+  palco: Array<string>;
+}
+export class Teatro {
+  nomeSpettacolo: string;
+  teatro: prenotazioni;
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  prenotazioni;
+  login: boolean = false;
+  admin: boolean = false;
+  prenotazioni: datiIn;
   spettacoli = new Array();
-  constructor(private AppDataService: AppDataService) {
+  spettacoloScelto: string;
+  nomeInserito: string;
+  constructor(private AppDataService: AppDataService) {}
+
+  //richiamata al change del <select>, restituisce il nome dello spettacolo
+  numeraSpettacolo($event) {
+    console.log($event.target.value);
+    this.spettacoloScelto = $event.target.value;
+  }
+  //il valore del campo input
+  inInput($event) {
+    this.nomeInserito = $event.target.value;
+  }
+  indietro() {
+    this.login = false;
+    this.spettacoli = [];
+  }
+  //richiede le prenotazioni e costruisce l'array con i nomi degli spettacoli
+  getPrenotazioni(admin) {
+    this.admin = admin;
+    this.login = true;
     this.AppDataService.getPrenotazioni$().subscribe({
       next: (res: string) => {
         this.prenotazioni = JSON.parse(res);
-        console.log(res);
-        console.log(this.prenotazioni);
-        for (let i in this.prenotazioni)
-          console.log(this.prenotazioni[i].nomeSpettacolo);
+        for (let elem in this.prenotazioni) {
+          this.spettacoli.push(this.prenotazioni[elem].nomeSpettacolo);
+        }
       },
       error: (err) =>
         console.error('Observer got an error: ' + JSON.stringify(err)),
     });
-  }
-  mostraPrenotazioni() {
     console.log(this.prenotazioni);
-
-    /*
-    this.AppDataService.getPrenotazioni$().subscribe({
-      next: (res: string) => {
-        console.log(res);
-      },
-      error: (err) =>
-        console.error('Observer got an error: ' + JSON.stringify(err)),
-    });*/
   }
+  mostraPrenotazioni() {}
 }
