8a610cea6de0a54a73db08cfb5ca025984832225
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-25 03:25:16 +0200

    5 files modified

--- src/app/teatro/teatro.component.ts
+++ src/app/teatro/teatro.component.ts
@@ -1,6 +1,34 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { Teatro } from '../app.component';
 
+export class Prenotazione {
+  nome: string;
+  fila: number;
+  posto: number;
+  constructor(nome: string, fila: number, posto: number) {
+    this.nome = nome;
+    this.fila = fila;
+    this.posto = posto;
+  }
+}
+
+export class PrenotazioneMultipla {
+  selezionati;
+  constructor() {
+    this.selezionati = [];
+  }
+  aggiungi(prenotazione) {
+    this.selezionati.push(prenotazione);
+  }
+  rimuovi(fila: number, posto: number) {
+    this.selezionati.map((old, i) => {
+      if (old.fila === fila && old.posto === posto) {
+        this.selezionati.splice(i, 1);
+      }
+    });
+  }
+}
+
 @Component({
   selector: 'app-teatro',
   templateUrl: './teatro.component.html',
@@ -8,9 +36,13 @@ import { Teatro } from '../app.component';
 })
 export class TeatroComponent implements OnInit {
   @Input() teatro: Teatro;
+  @Input() prenotazioneMultipla: PrenotazioneMultipla;
   constructor() {}
   mostraNome(this) {
     console.log(this.value);
+  }
+  confermaPrenotazioni() {
+    console.log(this.prenotazioneMultipla);
   }
   ngOnInit() {}
 }
--- src/app/zona-teatro/zona-teatro.component.ts
+++ src/app/zona-teatro/zona-teatro.component.ts
@@ -1,33 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { prenotazioni } from '../app.component';
-
-export class Prenotazione {
-  nome: string;
-  fila: number;
-  posto: number;
-  constructor(nome: string, fila: number, posto: number) {
-    this.nome = nome;
-    this.fila = fila;
-    this.posto = posto;
-  }
-}
-
-export class PrenotazioneMultipla {
-  selezionati;
-  constructor() {
-    this.selezionati = [];
-  }
-  aggiungi(prenotazione) {
-    this.selezionati.push(prenotazione);
-  }
-  rimuovi(fila: number, posto: number) {
-    this.selezionati.map((old, i) => {
-      if (old.fila === fila && old.posto === posto) {
-        this.selezionati.splice(i, 1);
-      }
-    });
-  }
-}
+import { Prenotazione, PrenotazioneMultipla } from '../teatro/teatro.component';
 
 @Component({
   selector: 'app-zona-teatro',
@@ -36,19 +8,21 @@ export class PrenotazioneMultipla {
 })
 export class ZonaTeatroComponent implements OnInit {
   @Input() prenotazioni;
-  @Input() nome;
-  @Input() rapido;
+  @Input() nome: string;
+  @Input() rapido: boolean;
+  @Input() selezionati: PrenotazioneMultipla;
   @Output() prenotazioniChange = new EventEmitter<Array<string>>();
-  selezionato;
-  selezionati: PrenotazioneMultipla;
+  @Output() selezionatiChange = new EventEmitter<PrenotazioneMultipla>();
+  selezionato: boolean;
   nuovaPrenotazione: Prenotazione;
   constructor() {}
-  selezionatoChange($event) {
+  isSelezionato($event: boolean) {
     this.selezionato = $event;
   }
+
   //Se rapida, prenota il posto
   //altrimenti aggiunge la prenotazione alla prenotazione multipla
-  prenota(fila, posto) {
+  prenota(fila: number, posto: number) {
     if (this.rapido) {
       this.prenotazioni[fila][posto] = this.nome;
       this.prenotazioniChange.emit(this.prenotazioni);
@@ -56,6 +30,7 @@ export class ZonaTeatroComponent implements OnInit {
       //crea una prenotazione multipla se non esiste e aggiunge la prima prenotazione
       if (this.selezionati === undefined) {
         this.selezionati = new PrenotazioneMultipla();
+        this.nuovaPrenotazione = new Prenotazione(this.nome, fila, posto);
         this.selezionati.aggiungi(this.nuovaPrenotazione);
       } else {
         if (!this.selezionato) {
@@ -65,8 +40,9 @@ export class ZonaTeatroComponent implements OnInit {
           this.selezionati.aggiungi(this.nuovaPrenotazione);
         }
       }
-      console.log(this.selezionati);
+      this.selezionatiChange.emit(this.selezionati);
     }
   }
+
   ngOnInit() {}
 }
