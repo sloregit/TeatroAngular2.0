e5a45514506e2bb1e9d312357a135b4fcde09afa
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-28 19:46:00 +0200

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -9,9 +9,9 @@ import { TeatroComponent } from './teatro/teatro.component';
 import { ZonaTeatroComponent } from './zona-teatro/zona-teatro.component';
 import { PulsanteComponent } from './pulsante/pulsante.component';
 import { GestoreSpettacoliComponent } from './gestore-spettacoli/gestore-spettacoli.component';
-import {SelezioneComponent} from './gestore-spettacoli/selezione/selezione.component'
+import { SelezioneComponent } from './gestore-spettacoli/selezione/selezione.component';
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     TeatroComponent,
--- src/app/gestore-spettacoli/gestore-spettacoli.component.ts
+++ src/app/gestore-spettacoli/gestore-spettacoli.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { formControl } from '@angular/core/schematics/migrations/typed-forms/util';
+import { FormControl } from '@angular/forms';
+
+import { spettacolo } from '../app.component';
 
 @Component({
   selector: 'app-gestore-spettacoli',
@@ -8,18 +12,55 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class GestoreSpettacoliComponent implements OnInit {
   @Input() arrayNomeSpettacoli;
   @Output() arrayNomeSpettacoliChange = new EventEmitter();
+  nomeSpettacolo: string;
   showOld: boolean = false;
   filePlatea: number;
   postiPlatea: number;
   filePalco: number;
   postiPalco: number;
   elementiPlatea: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  //postiPlatea: number[] = this.filePlatea;
   elementiPalco: number[] = [1, 2, 3, 4, 5, 6];
-  //postiPalco: number[] = this.filePalco;
+  nome = new FormControl('');
   constructor() {}
   confermaSpettacoli() {
-    console.log(this.filePlatea);
+    console.log(
+      ['Platea', this.filePlatea, this.postiPlatea],
+      ['Palco', this.filePalco, this.postiPalco]
+    );
+  }
+  nomeSpettacoloInput($event) {
+    console.log($event.target.value);
+  }
+  NewresetPrenotazioni() {
+    const prenotazioni: object = {
+      platea: Array(6)
+        .fill('fila')
+        .map(() =>
+          Array(10)
+            .fill('posto')
+            .map((val, posto) => {
+              return (val = 'x');
+            })
+        ),
+      palco: Array(4)
+        .fill('fila')
+        .map(() =>
+          Array(4)
+            .fill('posto')
+            .map((val, posto) => {
+              return (val = 'x');
+            })
+        ),
+    };
+    const sale = [
+      { nomeSpettacolo: 'spettacolo 1', teatro: prenotazioni },
+      { nomeSpettacolo: 'spettacolo 2', teatro: prenotazioni },
+      { nomeSpettacolo: 'spettacolo 3', teatro: prenotazioni },
+    ];
+    console.log(sale);
+    /*this.AppDBservice.SetPrenotazioni$(JSON.stringify(sale)).subscribe(
+      (val) => (this.conferma = 'Teatro resettato')
+    );*/
   }
   ngOnInit() {}
 }
